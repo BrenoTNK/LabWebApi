aaa0f7414b2894f0408730286b76c3a37079fe1b
Breno Tanaka <brenotanaka3@gmail.com>
2023-10-21 12:40:27 -0300

    feat: Token validation example

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./config/routes");
+const config = require("./config/env-config")
+const HapiJWT = require("hapi-auth-jwt2");
 
 
 const server = Hapi.server({
-  port: 8000,
-  host: 'localhost',
+  host: config.host,
+  port: config.port,
 });
 
 const plugins = [
@@ -15,5 +17,16 @@ const plugins = [
     }
   }
 ];
+
+const validate = async (decoded, req, h) => {
+  return { isValid: true };
+};
+
+if (config.jwt.enable === true) {
+  server.register(HapiJWT);
+  server.auth.strategy('jwt', 'jwt', { key: 'empresario', validate });
+  server.auth.default('jwt');
+}
+
 
 module.exports = { server, plugins };
